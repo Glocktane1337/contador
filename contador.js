5eeedef383f4855b1ade37a7ac3f0780e9eecb6a
Glocktane1337 <gsouza.gs617@gmail.com>
2021-09-29 22:23:53 -0300

    codigo funcionando v1.0

--- contador.js
+++ contador.js
@@ -1,55 +1,44 @@
-var opcao
-var contador;
 var readlinesync = require('readline-Sync');
 var escolher_numero = require('readline-Sync');
 var peca1 = 0;
 var peca2 = 0;
-var numero_selecionado = false
-var numero = parseInt(readlinesync.question ("=====================MENU=====================\n" +
-                                            "Pressione 0 para encerrar programa. \n"+
-                                            "Pressione 1 para acrescentar uma peca. \n"+
-                                            "Pressione 2 para mostrar a quantidade de pecas. \n"+
-                                            "===============================================\n")
-)
-/* var numero_reset = parseInt(readlinesync.question ("=====================MENU=====================\n" +
-                                            "Pressione 0 para encerrar programa. \n"+
-                                            "Pressione 1 para acrescentar uma peca. \n"+
-                                            "Pressione 2 para mostrar a quantidade de pecas. \n"+
-                                            "===============================================\n")) */
-//Declara as variáveis e faz a construção do menu principal.
+var numero
 
 do{
-   
-            switch(opcao) {
-                    case 1 :
-                        escolher_numero = parseInt(readlinesync.question ("Adicionar peca 1 ou peca 2?"));
-                    
-                        if (escolher_numero = 1) {
-                            peca1++
-                            console.log("Voce tem", peca1 , "pecas do tipo 1.") 
-                            break
-                        }
-                        else if (escolher_numero = 2){
-                            peca2++
-                            console.log("Voce tem", peca2 , "pecas do tipo 2.")
-                            break
-                        }
-                        
-                        else {
-                            console.log("Comando inválido.")
-                            numero_selecionado = false
-                        }     
+
+    numero = parseInt(readlinesync.question ("=====================MENU=====================\n" +
+                                            "Pressione 0 para encerrar programa. \n"+
+                                            "Pressione 1 para acrescentar uma peca. \n"+
+                                            "Pressione 2 para mostrar a quantidade de pecas. \n"+
+                                            "===============================================\n"))
+    switch(numero) {
+        
+        case 0 :
+            console.log("Encerrando...")
+            break;
+        
+        case 1 :
+            escolher_numero = parseInt(readlinesync.question ("Adicionar peca 1 ou peca 2?"));
+                if (escolher_numero == 1) {
+                    peca1++
+                    console.log("Voce tem", peca1 , "pecas do tipo 1.") 
+                }else if (escolher_numero == 2){
+                    peca2++
+                    console.log("Voce tem", peca2 , "pecas do tipo 2.")
+                    console.log(peca2)
+                }else {
+                    console.log("Comando inválido.")
                     break;
-                    
-                    case 2:
-                        console.log("Voce tem", peca1, "pecas do tipo 1 e", peca2, "pecas do tipo 2.")
-                        console.log (numero)
-                        break
+                }                     
+            break;            
+        case 2:
+            console.log("Voce tem", peca1, "pecas do tipo 1 e", peca2, "pecas do tipo 2.")
+            break
             
-                    default:
-                        console.log("Comando invalido.")
-                        break;
-            }
+        default:
+            console.log("Comando invalido.")
+            break;
+    }
                 
 }                                                             
 while (numero != 0) 
